4d30e6c9f5afb8d9a49771505d483d979dc0b1ac
许章 <xuzhang@xuzhangdeiMac.local>
2020-01-23 20:47:33 +0800

    first commit

---
+++ day2/day2-1.py
@@ -0,0 +1,16 @@
+"""
+使用变量保存数据并进行计算
+
+Version: 0.1
+Author: Robert Xu
+"""
+
+a = 321
+b = 123
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a % b)
+print(a**b)
---
+++ day2/day2-2.py
@@ -0,0 +1,9 @@
+"""
+使用type()  检查变量的类型
+
+Version:0.1
+Author: Robert Xu
+Date: 2020-01-23
+"""
+
+a=
