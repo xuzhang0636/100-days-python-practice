2f00718963bc75a7d0fa6d19c341c8dc908a2619
xuzhang0636 <363854939@qq.com>
2020-01-24 14:31:13 +0800

    finish day6 learning

---
+++ day6/module1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     module1
+   Description :
+   Author :        Robert Xu
+   date：          2020/1/24
+-------------------------------------------------
+   Change Activity:
+                   2020/1/24:
+-------------------------------------------------
+"""
+__author__ = 'Robert Xu'
+
+
+def foo():
+    print("hello, world")
---
+++ day6/test.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     test
+   Description :
+   Author :        Robert Xu
+   date：          2020/1/24
+-------------------------------------------------
+   Change Activity:
+                   2020/1/24:
+-------------------------------------------------
+"""
+__author__ = 'Robert Xu'
+
+import module1 as md1
+import module2 as md2
+
+md1.foo()
+md2.foo()
