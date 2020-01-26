d2a24d4eae9774b5e02e35ca4ffc99464120b917
xuzhang0636 <363854939@qq.com>
2020-01-26 22:34:42 +0800

    day13 practice

---
+++ day13/day13-2.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     day13-2.py
+   Description :   multiple task
+   Author :        Robert Xu
+   date：          2020/1/26
+-------------------------------------------------
+   Change Activity:
+                   2020/1/26:
+-------------------------------------------------
+"""
+__author__ = 'Robert Xu'
+
+from multiprocessing import Process
+from os import getpid
+from random import randint
+from time import time, sleep
+
+
+def download_task(filename):
+    print('启动下载进程，进程号[%d].' % getpid())
+    print('开始下载%s...' % filename)
+    time_to_download = randint(5, 10)
+    sleep(time_to_download)
+    print('%s下载完成！耗费了%d秒' % (filename, time_to_download))
+
+
+def main():
+    start = time()
+    p1 = Process(target=download_task, args=('Python从入门到住院.pdf',))
+    p1.start()
+    p2 = Process(target=download_task, args=('Peking Hot.avi',))
+    p2.start()
+    p1.join()
+    p2.join()
+    end = time()
+    print('总共耗费了%.2f秒' % (end - start))
+
+
+if __name__ == '__main__':
+    main()
